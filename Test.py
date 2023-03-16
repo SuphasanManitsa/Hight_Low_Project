77e94884d0a35f992722cc8a3a119cbb6336094a
Suphasan Manitsa <108540929+SuphasanManitsa@users.noreply.github.com>
2023-03-17 01:22:43 +0700

    ok

--- Test.py
+++ Test.py
@@ -2,13 +2,15 @@ from Hight_Low import Hight_Low
 import matplotlib.pyplot as plt
 hl = Hight_Low()
 balance = 100
-plt.figure(figsize = (12,12))
-for i in range(3):
-    plt.subplot(2,3,i+1)
-    a = []
-    for j in range(1,7):
-        a.append(hl.hight_low_whit_args(balance = balance,hit = 1,n = 100000,re = 1,a = [0,j],gong = [0,0]))
-    plt.xlim(0,7)
-    plt.plot(range(1,7),a)
-    plt.title(f'low {i + 1}')
-plt.show()
+# plt.figure(figsize = (12,12))
+# for i in range(3):
+#     plt.subplot(2,3,i+1)
+#     a = []
+#     for j in range(1,7):
+        # a.append(hl.hight_low_whit_args(balance = balance,hit = 1,n = 1000000000000,re = 1,a = [0,j],gong = [0,0]))
+#     plt.xlim(0,7)
+#     plt.plot(range(1,7),a)
+#     plt.title(f'low {i + 1}')
+# plt.show()
+# for i in range(1,7):
+#     print(hl.hight_low_whit_number(balance = balance,hit = 1,n = 10000000,re = 1,a = [0,i],gong = [0,0]))
